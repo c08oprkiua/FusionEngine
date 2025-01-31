e3e081034fb059b4c9ad9e4333a1225ef5a37531
c08oprkiua <85906487+c08oprkiua@users.noreply.github.com>
2025-01-30 22:32:17 -0700

    Working psp exporting from editor
    This can export a working EBOOT.PBP from the editor, featuring the project's name. Image/other metadata support coming soon:tm:

--- platform/psp/export/export.cpp
+++ platform/psp/export/export.cpp
@@ -4,7 +4,14 @@
 #include "tools/editor/editor_settings.h"
 #include "scene/resources/texture.h"
 #include "core/os/os.h"
+#include "core/os/file_access.h"
+#include "core/os/dir_access.h"
 #include "core/globals.h"
+
+
+static const char *sdk_path_var = "psp/base_sdk_path";
+static const char *pack_pbp_path = "/bin/pack-pbp";
+static const char *mksfo_path = "/bin/mksfo";
 
 class EditorExportPlatformPSP : public EditorExportPlatform {
     OBJ_TYPE(EditorExportPlatformPSP, EditorExportPlatform);
@@ -13,6 +20,8 @@ private:
 public:
 
 	void _get_property_list( List<PropertyInfo> *p_list) const;
+	bool _set(const StringName& p_name, const Variant& p_value);
+	bool _get(const StringName& p_name,Variant &r_ret) const;
 
 	virtual String get_name() const;
 	virtual ImageCompression get_image_compression() const;
@@ -35,8 +44,17 @@ void EditorExportPlatformPSP::_get_property_list( List<PropertyInfo> *p_list) co
 	p_list->push_back(PropertyInfo(Variant::BOOL, "data/embed_pck"));
 	p_list->push_back(PropertyInfo(Variant::STRING, "launcher/background", PROPERTY_HINT_FILE, "png"));
 	p_list->push_back(PropertyInfo(Variant::STRING, "launcher/icon", PROPERTY_HINT_FILE, "png"));
-	p_list->push_back(PropertyInfo(Variant::STRING, "launcher/sound", PROPERTY_HINT_FILE, "mp3"));
+	p_list->push_back(PropertyInfo(Variant::STRING, "launcher/sound", PROPERTY_HINT_FILE, "at3"));
 }
+
+bool EditorExportPlatformPSP::_set(const StringName& p_name, const Variant& p_value){
+
+}
+
+bool EditorExportPlatformPSP::_get(const StringName& p_name,Variant &r_ret) const{
+
+}
+
 
 String EditorExportPlatformPSP::get_name() const {
     return "Playstation Portable";
@@ -54,9 +72,49 @@ Ref<Texture> EditorExportPlatformPSP::get_logo() const {
 }
 
 bool EditorExportPlatformPSP::can_export(String *r_error) const {
-	String err("PSP exporting is not set up yet!");
-	r_error = &err;
-    return false;
+	String err = "";
+
+	bool valid = true;
+
+	String sdk_path = EditorSettings::get_singleton()->get(sdk_path_var);
+
+	Error dir_err;
+	DirAccess::open(sdk_path, &dir_err);
+	if (dir_err != OK){
+		valid = false;
+		err += "Can't open the PSPSDK path, please set this in Editor Settings.\n";
+	} else {
+		if (not FileAccess::exists(sdk_path + mksfo_path)){
+			valid = false;
+			err += "Can't find mksfo in the PSPSDK.\n";
+		}
+		if (not FileAccess::exists(sdk_path + pack_pbp_path)){
+			valid = false;
+			err += "Can't find pack-pbp in the PSPSDK.\n";
+		}
+	}
+
+	String exe_path = EditorSettings::get_singleton()->get_settings_path()+"/templates/";
+
+	bool debug_found = FileAccess::exists(exe_path + "psp_debug.32");
+	bool release_found = FileAccess::exists(exe_path + "psp_release.32");
+
+	if (not debug_found or not release_found) {
+		valid=false;
+		err +="No export templates found.\nDownload and install export templates.\n";
+	} else if (not debug_found){
+		err += "No debug export template found.\n";
+	} else if(not release_found){
+		err += "No release export template found.\n";
+	}
+
+	//TODO: Validity checks on the icons and etc.
+
+	if (r_error){
+		*r_error=err;
+	}
+
+    return valid;
 };
 
 String EditorExportPlatformPSP::get_binary_extension() const {
@@ -66,21 +124,39 @@ String EditorExportPlatformPSP::get_binary_extension() const {
 Error EditorExportPlatformPSP::export_project(const String& p_path,bool p_debug,bool p_dumb){
 	//This is not a complete implementation, just a start
 	List<String> args;
+	int retcode;
+	Error run_err;
+
+	String sdk_path = EditorSettings::get_singleton()->get(sdk_path_var);
 
 	//Make PARAM.SFO
-	args.push_back(Globals::get_singleton()->get("application/name"));
+
+	String application_name = Globals::get_singleton()->get("application/name");
+
+	if (application_name.empty()){
+		application_name = "Fusion Engine";
+	}
+
+	args.push_back(application_name);
 
 	String sfo_path = EditorSettings::get_singleton()->get_settings_path() + "/tmp/PARAM.SFO";
 
 	args.push_back(sfo_path);
 
-	OS::get_singleton()->execute("mksfo", args, true);
+	run_err = OS::get_singleton()->execute(sdk_path + mksfo_path, args, true, NULL, NULL, &retcode);
+
+	if (run_err or retcode != 0){
+		print_line("Error in running mksfo: " + String::num(retcode));
+		return ERR_CANT_CREATE;
+	}
+
+	retcode = 0;
 
 	args.clear();
 
 	//Make EBOOT.PBP
 
-	args.push_back(p_path + "EBOOT.PBP"); //output.pbp
+	args.push_back(p_path); //output.pbp
 	args.push_back(sfo_path); //param.sfo
 
 	args.push_back("NULL"); //icon0.png
@@ -89,17 +165,27 @@ Error EditorExportPlatformPSP::export_project(const String& p_path,bool p_debug,
 	args.push_back("NULL"); //pic1.png
 	args.push_back("NULL"); //snd0.at3
 
+	String exe_path = EditorSettings::get_singleton()->get_settings_path()+"/templates/";
+
 	if (p_debug){ //debug template
-		args.push_back("NULL");
+		args.push_back(exe_path + "psp_debug.32");
 	} else { //release template
-		args.push_back("NULL");
+		args.push_back(exe_path + "psp_release.32");
 	}
 	args.push_back("NULL"); //data.psar
 
-	OS::get_singleton()->execute("pack-pbp", args, true);
+	run_err = OS::get_singleton()->execute(sdk_path + pack_pbp_path, args, true, NULL, NULL, &retcode);
+
+	if (run_err or retcode != 0){
+		print_line("Error in running pack-pbp: " + String::num(retcode));
+		return ERR_CANT_CREATE;
+	}
+
+	return OK;
 };
 
 void register_psp_exporter(){
+	EDITOR_DEF(sdk_path_var, "");
     Ref<EditorExportPlatformPSP> exporter = Ref<EditorExportPlatformPSP>(memnew(EditorExportPlatformPSP));
 	EditorImportExport::get_singleton()->add_export_platform(exporter);
 }
